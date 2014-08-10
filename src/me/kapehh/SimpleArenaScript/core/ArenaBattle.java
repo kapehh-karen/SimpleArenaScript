80350ba429158ba65ec550bf2f95667460849468
kapehh-karen <git@kapehh.net>
2014-08-10 22:11:08 +0400

    add classes

---
+++ src/me/kapehh/SimpleArenaScript/core/ArenaBattle.java
@@ -0,0 +1,46 @@
+package me.kapehh.SimpleArenaScript.core;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Karen on 10.08.2014.
+ */
+public class ArenaBattle {
+    private int configTime;
+
+    private List<Player> players = new ArrayList<Player>();
+    private int timeLeft;
+
+    public ArenaBattle(int configTime) {
+        this.configTime = configTime;
+        reset();
+    }
+
+    public void reset() {
+        timeLeft = configTime;
+        players.clear();
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public boolean inArena(Player player) {
+        return players.contains(player);
+    }
+
+    public int getTimeLeft() {
+        return timeLeft;
+    }
+
+    public void downTime() {
+        timeLeft--;
+    }
+
+    public boolean hasTime() {
+        return timeLeft > 0;
+    }
+}
---
+++ src/me/kapehh/SimpleArenaScript/core/ArenaQueue.java
@@ -0,0 +1,27 @@
+package me.kapehh.SimpleArenaScript.core;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Karen on 10.08.2014.
+ */
+public class ArenaQueue {
+    private String name;
+    private Location locationIn;
+    private Location locationLobby;
+    private List<Location> spawnLocations = new ArrayList<Location>();
+    private int needPlayers;
+    private List<Player> playerList = new ArrayList<Player>();
+
+    public ArenaQueue(String name, Location locationIn, Location locationLobby, List<Location> spawnLocations, int needPlayers) {
+        this.name = name;
+        this.locationIn = locationIn;
+        this.locationLobby = locationLobby;
+        this.spawnLocations = spawnLocations;
+        this.needPlayers = needPlayers;
+    }
+}
